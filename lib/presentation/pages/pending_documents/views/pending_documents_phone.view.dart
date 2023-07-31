f9b6ee645620dbf349ceff8c2762245f1130a03e
Carlos H. Aguirre <64186814+carlitosxx@users.noreply.github.com>
2023-07-31 01:28:38 -0500

    feat:fix ui

--- lib/presentation/pages/pending_documents/views/pending_documents_phone.view.dart
+++ lib/presentation/pages/pending_documents/views/pending_documents_phone.view.dart
@@ -27,31 +27,75 @@ class PendingDocumentsPhoneViewState
           title: Text(kTitle.i18n),
         ),
         drawer: const MyDrawer(),
-        body: CustomScrollView(
-          slivers: [
-            SliverPadding(
-              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-              sliver: SliverToBoxAdapter(
-                child: TextfieldWidget(
-                  controller: search,
-                  inputType: TextInputType.text,
-                  hint: kSearchDocument.i18n,
-                  isPassword: false,
-                  hasIcon: true,
-                  icon: Icons.search,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: CustomScrollView(
+            slivers: [
+              SliverPadding(
+                padding: const EdgeInsets.only(bottom: 16),
+                sliver: SliverToBoxAdapter(
+                  child: TextfieldWidget(
+                    controller: search,
+                    inputType: TextInputType.text,
+                    hint: kSearchDocument.i18n,
+                    isPassword: false,
+                    hasIcon: true,
+                    icon: Icons.search,
+                  ),
                 ),
               ),
-            ),
-            SliverPadding(
-              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-              sliver: SliverToBoxAdapter(
-                child: Text(
-                  kResults.i18n,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+              SliverPadding(
+                padding: const EdgeInsets.only(bottom: 16),
+                sliver: SliverToBoxAdapter(
+                  child: Text(
+                    kResults.i18n,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            )
-          ],
+              SliverList.builder(
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 20),
+                    margin:
+                        const EdgeInsets.only(bottom: 16, left: 16, right: 16),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.background,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .onBackground
+                              .withOpacity(0.2),
+                          blurRadius: 2,
+                          offset: const Offset(1, 1),
+                        )
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/pdf.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                            const SizedBox(width: 20),
+                          ],
+                        ),
+                        const Divider(),
+                        Text('asdasd123')
+                      ],
+                    ),
+                  );
+                },
+                itemCount: 10,
+              )
+            ],
+          ),
         ),
       ),
     );
