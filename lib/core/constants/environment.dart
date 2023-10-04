c533db7c82db935c391fc46ad8687bafd7b2a7ff
Carlos Aguirre <carlosaguirre317@gmail.com>
2023-10-03 20:56:07 -0500

    fix:constant apiUrl

--- lib/core/constants/environment.dart
+++ lib/core/constants/environment.dart
@@ -1,4 +1,4 @@
 class Environment {
-  // static String apiUrl = 'https://www.davidocs.com/API/movil/v1/api_rest.php/';
-  static String apiUrl = 'https://app.davidocs.com/API/test1/v1/api_rest.php/';
+  static String apiUrl = 'https://www.davidocs.com/API/movil/v1/api_rest.php/';
+  // static String apiUrl = 'https://app.davidocs.com/API/test1/v1/api_rest.php/';
 }
